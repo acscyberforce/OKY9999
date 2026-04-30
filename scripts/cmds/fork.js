047ffa8a2c9dc9bb8bf57f131e2d33be954e01ca
acscyberforce <pck871935@gmail.com>
2026-05-01 05:01:43 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   onStart: async function({ message }) {
-    const text = "https://github.com/rocky-bot-320/Messenger-goat-bot.git";
+    const text = "𝐻𝐴 𝐻𝐴 𝐹𝑈𝐶𝐾 𝑌𝑂𝑈𝑅 𝐷𝐴𝐷 𝐼𝑀 𝑇𝑂𝑁𝑀𝑂𝑌 𝐵𝑂𝑇 🙃";
     
     message.reply(text);
   }
